ff9654a7fb4972d62df1c215a8afbfb40a2e60fb
lisiming228 <1105002960@qq.com>
2020-08-23 22:11:04 +0800

    first

---
+++ application/config.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+    "database"=>array(
+        "host"=>"localhost",
+        "username"=>"root",
+        "password"=>"123456",
+        "dbname"=>"test",
+        "port"=>"3306",
+    ),
+    "smarty"=>array(
+        "templateDir"=>TPL_PATH,
+        "compileDir"=>COMPILE_PATH,
+        "cacheDir"=>CACHE_PATH,
+    ),
+
+
+);
